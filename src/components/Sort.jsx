8f90e9867dbc00c5ba793e400eaafce372a9b493
Robot <aomaymistov@gmail.com>
2023-03-07 16:26:07 +0300

    added handleClickOutside for closing popup

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setSort } from "../redux/slice/filterSlice";
 
@@ -14,6 +14,8 @@ export const sortList = [
 function Sort() {
   const dispatch = useDispatch();
   const sort = useSelector((state) => state.filter.sort);
+  const sortRef = useRef();
+
   const [openPopUp, setOpenPopUp] = useState(false);
 
   const onClickSortItem = (obj) => {
@@ -21,8 +23,20 @@ function Sort() {
     setOpenPopUp(false);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      const composed = event.composedPath().includes(sortRef.current);
+      if (!composed) {
+        setOpenPopUp(false);
+      }
+    };
+    document.body.addEventListener("click", handleClickOutside);
+
+    return () => document.body.removeEventListener("click", handleClickOutside);
+  }, []);
+
   return (
-    <div className="sort">
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
         <svg
           width="10"
